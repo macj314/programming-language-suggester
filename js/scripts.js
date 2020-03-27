c9dd088b8802c45ec8709186cc9fb29b053cc215
unknown <macjay314@gmail.com>
2020-03-27 13:35:52 -0700

    Encapsulate input fields inside of rows, panels, and columns.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $("form#form-survey").submit(function(){
-    event.preventDefault();
+
     var inputLanguage = $("#input-language").val();
     var environment = parseInt($("input:radio[name=environment]:checked").val());
     var color = parseInt($("input:radio[name=color]:checked").val());
@@ -12,9 +12,20 @@ $(document).ready(function(){
     $("#language-prefix").show();
 
     if(color === 2){
-    $("#ruby").show();
-    } else {
+      $("#ruby").show();
+
+    } else if (cat === 5 || caffeine > 3){    
+      $("#python").show();
+
+    } else if (environment === 2){
+      $("#language-prefix").hide();
+      $("#ruby").show();
+      $("#js").show();
+      $("#python").show();
+
+    }  else {
       $("#js").show();
     }
+    event.preventDefault();
   })
 })
