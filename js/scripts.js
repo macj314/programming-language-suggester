9eef2fd75c72cc61f17c006d129f36adc823d180
unknown <macjay314@gmail.com>
2020-03-27 10:01:11 -0700

    Attempt to add select fields.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $("form#form-survey").submit(function(){
     event.preventDefault();
-    var goals = $("#goals").val();
+    var inputLanguage = $("#input-language").val();
     var environment = parseInt($("input:radio[name=environment]:checked").val());
     var color = parseInt($("input:radio[name=color]:checked").val());
 
