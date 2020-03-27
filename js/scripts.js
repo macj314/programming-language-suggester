b89df92c576f5b445a60f8a82306ad9a871fd363
unknown <macjay314@gmail.com>
2020-03-27 09:45:24 -0700

    Add the 'color' radio buttons. Set their values to numbers. Then parseInt those values from string to int.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
   $("form#form-survey").submit(function(){
     event.preventDefault();
     var goals = $("#goals").val();
-    $("#input").text(goals);
+    var environment = parseInt($("input:radio[name=environment]:checked").val());
+    var color = parseInt($("input:radio[name=color]:checked").val());
+
+    $("#input").text(environment);
   })
 })
